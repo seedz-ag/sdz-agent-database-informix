3a5a2b6e0e389b901a3d16a642972170727653e3
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-10-05 17:00:31 -0300

    repository

--- dist/repository.js
+++ dist/repository.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sdz_agent_types_1 = require("sdz-agent-types");
-let query = "";
 class Repository extends sdz_agent_types_1.AbstractRepository {
 }
 exports.default = Repository;
--- src/repository.ts
+++ src/repository.ts
@@ -1,6 +1,3 @@
-import fs from "fs";
 import { AbstractRepository } from "sdz-agent-types";
-
-let query: string = "";
 
 export default class Repository extends AbstractRepository {}
